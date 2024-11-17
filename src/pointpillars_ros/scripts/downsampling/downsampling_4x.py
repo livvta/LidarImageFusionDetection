317c0e14c16ca07d5e95276068cff0810690a48b
livvta-HX80G <harris1701@163.com>
2024-11-18 01:04:52 +0800

    点云降采样24.11.18

---
+++ src/pointpillars_ros/scripts/downsampling/downsampling_4x.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+'''
+downsampling_4x.py
+==================
+Version: 1.0
+Last Modified: 2024-11-17 23:46
+
+功能：
+- 根据激光雷达的线束特性, 对点云数据进行4倍下采样
+'''
+
+import numpy as np
+import os
+from preprocessing_utils import read_bin_file, write_bin_file
+
+
+def find_beam_ends(points):
+    """
+    判断线数及每条激光线的最后一个点
+    @param points: np.array [N, 4] 原始的点云数据
+    """
+    cloud_size = len(points)  # 点云数量
+    beam_end_indices = []  # 每条线的最后一个点的索引
+    beam_count = 0    # 线数
+    prev_angle = None  # 上一个点的方位角
+    prev2_angle = None  # 上上个点的方位角
+    is_discarded = True # 是否舍弃当前点
+    discard_count = 0 # 初始化舍弃计数
+
+    for i in range(cloud_size):
+        # 计算当前点的方位角
+        angle = np.arctan2(points[i, 1], -points[i, 0]) * 180 / np.pi
+
+        # 初始化
+        if i == 0:
+            prev_angle = angle
+            prev2_angle = angle
+
+        # 前十个点不进入下一线束的判断, 避免噪声影响
+        if is_discarded and discard_count < 10:
+            discard_count += 1
+        else:
+            is_discarded = False
+
+        # 判断是否属于下一条线
+        if (angle - prev_angle) >= 90 and not is_discarded and (angle - prev2_angle) >= 90:
+            beam_count += 1  # 线数加1
+            beam_end_indices.append(i)  # 记录当前线的最后一个点的索引
+
+            is_discarded = True  # 舍弃当前点
+            discard_count = 0  # 重置舍弃计数
+
+        # 更新上一个点和上上个点的方位角
+        prev2_angle = prev_angle
+        prev_angle = angle
+
+    beam_end_indices.append(cloud_size - 1)  # 将最后一个点的索引加入
+
+    if beam_count != 63:
+        print("Warimg: beams not equal to 64.  beams:", beam_count)
+    
+    return beam_end_indices
+
+
+def downsample_pointcloud(points, beam_end_indices):
+    """
+    根据分线的index结果, 按四条线选择一条, 生成新点云
+    @param points: np.array [N, 4] 原始的点云数据
+    @param beam_end_indices: list 每条线的最后一个点的索引
+    @return: np.array [M, 4] 下采样后的点云数据
+    """
+    selected_points = []
+    for i in range(0, len(beam_end_indices), 4):
+        start_idx = beam_end_indices[i]
+        end_idx = beam_end_indices[i + 1]
+        selected_points.extend(points[start_idx:end_idx])
+    return np.array(selected_points)
+
+
+def process_folder(input_folder, output_folder):
+    """
+    处理文件夹下的所有bin文件
+    @param input_folder: 输入文件夹
+    @param output_folder: 输出文件夹
+    """
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for file_name in os.listdir(input_folder):
+        if file_name.endswith('.bin'):
+            input_file_path = os.path.join(input_folder, file_name)
+            output_file_path = os.path.join(output_folder, file_name)
+
+            points = read_bin_file(input_file_path)
+            beam_end_indices = find_beam_ends(points)
+            selected_points = downsample_pointcloud(points, beam_end_indices)
+            write_bin_file(output_file_path, selected_points)
+            print(f"Processed {file_name}")
+
+
+if __name__ == "__main__":
+    input_folder = 'c:/Users/livvta/OneDrive/EDU/graduation_project/detection_ws/src/pointpillars_ros/scripts/downsampling/data'
+    output_folder = 'c:/Users/livvta/OneDrive/EDU/graduation_project/detection_ws/src/pointpillars_ros/scripts/downsampling/data_output'
+    process_folder(input_folder, output_folder)
---
+++ src/pointpillars_ros/scripts/downsampling/pointcloud_analysis.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+'''
+pointcloud_analysis.py
+=================
+Version: 1.0
+Last Modified: 2024-11-18 01:11
+
+可视化分析点云数据的方位角和高度角
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from preprocessing_utils import read_bin_file
+
+def analyze_pointcloud_data(data):
+    '''
+    @param data: np.ndarray  [N, 4]  点云数据
+    '''
+    hori_angle = np.arctan2(data[:,0], data[:,1]) / np.pi * 180  # 计算方位角
+    elev_angle = np.arctan2(data[:,2], np.sqrt(data[:,0]**2 + data[:,1]**2)) / np.pi * 180  # 计算高度角
+
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.plot(hori_angle)
+    plt.xlabel('Point Index')
+    plt.ylabel('Azimuthal Angle (Degrees)')
+    plt.title('Azimuthal Angle Analysis')
+    plt.ylim(-220, 220)
+    plt.yticks(np.arange(-180, 181, 90))
+
+    plt.subplot(2, 1, 2)
+    plt.plot(elev_angle)
+    plt.xlabel('Point Index')
+    plt.ylabel('Elevation Angle (Degrees)')
+    plt.title('Elevation Angle Analysis')
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    file_paths = [
+        'c:/Users/livvta/OneDrive/EDU/graduation_project/detection_ws/src/pointpillars_ros/scripts/downsampling/data_output/000001.bin',
+        'c:/Users/livvta/OneDrive/EDU/graduation_project/detection_ws/src/pointpillars_ros/scripts/downsampling/data/000000.bin'
+    ]
+    for file_path in file_paths:
+        point_cloud = read_bin_file(file_path)
+        analyze_pointcloud_data(point_cloud)
+
